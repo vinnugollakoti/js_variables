b1a1badade6eec6e9c3407a877db237a5e4cffae
vinnugollakoti <145451307+vinnugollakoti@users.noreply.github.com>
2023-10-13 09:47:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js_variables
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/vinnugollakoti/js_variables)
---
+++ script.js
@@ -0,0 +1,35 @@
+// Do not edit the below 2 lines of code
+const nameContainer = document.getElementById('name-container');
+const ageContainer = document.getElementById('age-container');
+
+// Challenge 1: Building Profile
+// Create a variable using the keyword "const", with an identifier as "myName" and assign your name as its value.
+
+// your code goes here
+const myName = "Vinay"
+// Do not edit the below code, uncomment the below line when you are working on challenge 1
+nameContainer.innerHTML = myName;
+
+// Challenge 2: Create a variable with the identifier "myAge" using the appropriate keyword between "const and var" keep one thing in your mind, your age keeps changing, so while declaring the variable ensure that the keyword alows you to change the value of the myAge variable after your birthday.
+
+// your code goes here
+const myAge = 18
+// Do not edit the below code, uncomment the below line when you are working on challenge 2
+ageContainer.innerHTML = myAge;
+
+// Challenge 3: This is a debugging challenge. You can find a few variable declarations below, some of which are correct, and some have a few bugs. Try fixing them. Please look at the console to find the bugs. Uncomment the below lines of code one by one while working on this challenge.
+
+const Question="This is the first debugging question. If you fix this question, you will be able to see this text on your console";
+console.log(Question);
+
+const secondQuestion="This is the second debugging question, if you fix this question you will be able to see this text on your console";
+console.log(secondQuestion);
+
+const thirdQuestion="This is the third debugging question, if you fix this question you will be able to see this text on your console";
+console.log(thirdQuestion);
+
+const fourthQuestion="This is the fourth debugging question, if you fix this question you will be able to see this text on your console";
+console.log(fourthQuestion);
+
+const fifthquestion="This is the fifth debugging question, if you fix this question you will be able to see this text on your console";
+console.log(fifthquestion);
